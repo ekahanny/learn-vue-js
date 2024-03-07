4e70f5c8007d25a0b1081bec6db796629e41a1df
ekahanny <ekahnny@gmail.com>
2024-03-07 11:26:48 +0800

    mengelola virtual dom

---
+++ Chapter 1/app.js
@@ -0,0 +1,9 @@
+// Fitur Vue.js -> Mengelola Virtual DOM
+
+// Membuat instance baru
+let apps = new Vue({
+  el: "#app", // Menambahkan property elemen sebagai selector
+  data: {
+    name: "Vue.js", // Digunakan untuk menyimpan data
+  },
+});
