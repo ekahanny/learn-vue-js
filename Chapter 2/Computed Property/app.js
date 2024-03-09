9c2a103993aa6ab3e69ff175fbf5cd61d3f2ddba
ekahanny <ekahnny@gmail.com>
2024-03-09 22:01:41 +0800

    Perbedaan Computed & Method

--- Chapter 2/Computed Property/app.js
+++ Chapter 2/Computed Property/app.js
@@ -5,10 +5,33 @@ let app = new Vue({
   },
   computed: {
     slugetize: function () {
-      return this.slugText
-        .toLowerCase()
-        .replace(/[^\w ]+/g, "")
-        .replace(/ +/g, "-");
+      return (
+        this.slugText
+          .toLowerCase()
+          .replace(/[^\w ]+/g, "")
+          .replace(/ +/g, "-") +
+        "-" +
+        this.now
+      );
+    },
+  },
+  methods: {
+    now: function () {
+      let date = new Date();
+      return (
+        String(date.getHours()) +
+        String(date.getMinutes()) +
+        String(date.getSeconds())
+      );
     },
   },
 });
+
+/*
+Perbedaan Method dan Computed
+Computed value ter-cached oleh browser, sedangkan methods tidak.
+Jadi bila dalam sebuah view muncul 100 variable yg sama, computed
+hanya menjalankan fungsi 1x saja (selama tidak ada perubahan),
+sedangkan methods akan menjalankan fungsi tersebut 100x. Selain itu,
+method sebaiknya digunakan bila functionnya memiliki parameter.
+*/
