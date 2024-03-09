e77d4c56604f5b55ce2d8ee5ee5c64c1ba486868
ekahanny <ekahnny@gmail.com>
2024-03-09 21:36:27 +0800

    menambahkan materi computed property

---
+++ Chapter 2/Computed Property/app.js
@@ -0,0 +1,14 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    slugText: "The Quick #(*!&@ Brown 29",
+  },
+  computed: {
+    slugetize: function () {
+      return this.slugText
+        .toLowerCase()
+        .replace(/[^\w ]+/g, "")
+        .replace(/ +/g, "-");
+    },
+  },
+});
