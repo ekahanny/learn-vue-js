b15ff5d310b64a456cc4c582a8e307ebc687d4d9
ekahanny <ekahnny@gmail.com>
2024-03-11 22:08:55 +0800

    Mengatur Style dengan JS

--- Chapter 3/app.js
+++ Chapter 3/app.js
@@ -23,6 +23,23 @@ let app = new Vue({
     },
   },
   methods: {
+    before: function (el) {
+      el.className = "d-none";
+    },
+    enter: function (el) {
+      let delay = el.dataset.index * 100;
+      setTimeout(function () {
+        el.className =
+          "row d-flex mb-3 align-items-center animated fadeInRight";
+      }, delay);
+    },
+    leave: function (el) {
+      let delay = el.dataset.index * 100;
+      setTimeout(function () {
+        el.className =
+          "row d-flex mb-3 align-items-center animated fadeOutRight";
+      }, delay);
+    },
     addItem: function (item) {
       this.cart.push(item);
     },
