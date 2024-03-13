91450e3ee8991954ef5de70a890dd6cc7785b697
ekahanny <ekahnny@gmail.com>
2024-03-13 14:36:00 +0800

    Menggunakan Fungsi Filter pada Vue

---
+++ Chapter 4/app.js
@@ -0,0 +1,59 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    minimum: 2.7,
+    products: null,
+    cart: [],
+    styles: {
+      label: ["font-weight-bold", "mr-2"],
+      inputWidth: 60,
+      sliderStatus: false,
+    },
+  },
+  mounted: function () {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((response) => response.json())
+      .then((data) => {
+        this.products = data;
+      });
+  },
+  // Filters -> Menampilkan/Memproses Suatu Data
+  filters: {
+    currencyFormat: function (value) {
+      return "Rp" + Number.parseFloat(value).toFixed(2);
+    },
+  },
+  computed: {
+    sliderState: function () {
+      return this.styles.sliderStatus ? "d-flex" : "d-none";
+    },
+  },
+  methods: {
+    before: function (el) {
+      el.className = "d-none";
+    },
+    enter: function (el) {
+      let delay = el.dataset.index * 100;
+      setTimeout(function () {
+        el.className =
+          "row d-flex mb-3 align-items-center animated fadeInRight";
+      }, delay);
+    },
+    leave: function (el) {
+      /*
+          dataset digunakan utk mengakses semua atribut 
+          data yg didefinisikan pada elemen html. intinya,
+          el.dataset.index digunakan utk mengakses index 
+          
+        */
+      let delay = el.dataset.index * 100;
+      setTimeout(function () {
+        el.className =
+          "row d-flex mb-3 align-items-center animated fadeOutRight";
+      }, delay);
+    },
+    addItem: function (item) {
+      this.cart.push(item);
+    },
+  },
+});
