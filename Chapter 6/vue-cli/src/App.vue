0bce8c50987f6b327fc8b9daf15dbdb7c471fcf1
ekahanny <ekahnny@gmail.com>
2024-03-17 11:30:55 +0800

    Implementasi Modul pada Vue

--- Chapter 6/vue-cli/src/App.vue
+++ Chapter 6/vue-cli/src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <!-- satu component memiliki satu div utama saja -->
+  <div id="app" class="container mt-5">
+    <h1>IDShop</h1>
+    <p class="animated fadeInRight">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat a
+      dignissimos laudantium id? Eius laboriosam sint recusandae molestias
+      voluptatum eligendi rem dolores labore perferendis dicta! Voluptates
+      magnam tenetur rerum facere.
+    </p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    FontAwesomeIcon,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
